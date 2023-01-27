157edfa89e83daeb15f773a8beb79940ed5ee7c9
Roman Mikaelyan <romanmik@yandex.ru>
2023-01-27 18:41:21 +0300

    add blog section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+// swiper
+import Swiper, { Navigation } from "swiper";
 // styles
 import "./fonts/fonts.scss";
 import "./style.scss";
+import "swiper/css";
 
 function init() {
-  console.log("init");
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation],
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-right",
+      prevEl: ".swiper-button-left",
+    },
+  });
 }
 
 document.addEventListener("DOMContentLoaded", init);
